210848f8ef0bd09e191fdf5e96b953dc4fd24c5c
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2024-06-30 13:42:21 +0500

    resolved issues in context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,31 +13,32 @@ import Register from "./Components/Register";
 import CartProduct from "./Components/CartProduct";
 import "./App.css";
 import { CartProvider } from "./Components/CartContext";
-
+import { AuthProvider } from "./Components/AuthContext";
 function App() {
   const noHeaderFooterRoutes = ["/login", "/register"];
   const currentPath = window.location.pathname;
-
   return (
-    <CartProvider>
-      {!noHeaderFooterRoutes.includes(currentPath) && <Header />}
-      {!noHeaderFooterRoutes.includes(currentPath) && <HeroSection />}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/cart" element={<CartProduct />} />
-          <Route path="/product" element={<FeaturedProducts />} />
-          <Route path="/new-arrivals" element={<NewArrivals />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-      {!noHeaderFooterRoutes.includes(currentPath) && <Categories />}
-      {!noHeaderFooterRoutes.includes(currentPath) && <Offers />}
-      {!noHeaderFooterRoutes.includes(currentPath) && <Newsletter />}
-      {!noHeaderFooterRoutes.includes(currentPath) && <NewArrivals />}
-      {!noHeaderFooterRoutes.includes(currentPath) && <Testimonals />}
-      {!noHeaderFooterRoutes.includes(currentPath) && <Footer />}
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        {!noHeaderFooterRoutes.includes(currentPath) && <Header />}
+        {!noHeaderFooterRoutes.includes(currentPath) && <HeroSection />}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/cart" element={<CartProduct />} />
+            <Route path="/product" element={<FeaturedProducts />} />
+            <Route path="/new-arrivals" element={<NewArrivals />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+        {!noHeaderFooterRoutes.includes(currentPath) && <Categories />}
+        {!noHeaderFooterRoutes.includes(currentPath) && <Offers />}
+        {!noHeaderFooterRoutes.includes(currentPath) && <Newsletter />}
+        {!noHeaderFooterRoutes.includes(currentPath) && <NewArrivals />}
+        {!noHeaderFooterRoutes.includes(currentPath) && <Testimonals />}
+        {!noHeaderFooterRoutes.includes(currentPath) && <Footer />}
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
